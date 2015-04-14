5fba2d7d22f66f359c8019ff13c26a9f06dda71d
Reid Baldwin & Kelly de Vries <student@epicodus.com>
2015-04-14 11:53:34 -0700

    Add columns and functionality  to save triangles by stype.

--- js/scripts.js
+++ js/scripts.js
@@ -10,22 +10,28 @@ $(document).ready(function() {
                          side3: inputted_side3,
                          type: function() {
                              if(this.side1 >= this.side2 + this.side3 || this.side2 >= this.side1 + this.side3 || this.side3 >= this.side1 + this.side2) {
+
                                  return "DAS IST KEIN TRIANGLE!!!";
                              }
                              else if(this.side1 === this.side2 &&  this.side2 === this.side3) {
+                                 $("ul.equilaterals").append("<li><span class = 'equilateral'>" + this.side1 + " ," + this.side2+ " ," +this.side3 + "</span></li>" );
                                  return "DAS IST EQUILATERAL TRIANGLE!!!";
                              }
                              else if((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3) || (this.side2 === this.side3 && this.side1 !== this.side3)) {
+                                 $("ul.isosceles").append("<li><span class = 'isoscele'>" + this.side1 + " ," + this.side2+ " ," +this.side3 + "</span></li>" );
                                  return "DAS IST ISOCELES TRIANGLE!!!";
                              }
                              else if(this.side1 !== this.side2 && this.side2 !== this.side3) {
+                                 $("ul.scalenes").append("<li><span class = 'scalene'>" + this.side1 + " ," + this.side2+ " ," +this.side3 + "</span></li>" );
                                  return "DAS IST SCALENE TRIANGLE!!!";
                              }
 
 
     }};
 
+
     $(".result").text(triangle.type());
+
 
     $("input#sidea").val("");
     $("input#sideb").val("");
